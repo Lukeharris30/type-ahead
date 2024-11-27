c667672b8a2e27b33b1688a14f1293ba8f5cb33a
Luke Harris <8006624+Lukeharris30@users.noreply.github.com>
2024-11-27 09:19:26 -0700

    fix:  label-click

--- src/utlis/renderListOptions.ts
+++ src/utlis/renderListOptions.ts
@@ -1,11 +1,12 @@
 import { clearListOptions } from "./clearListOptions"
 import { attachEventListenerToListOptions } from "./attachEventListenerToOptions"
+
 export function renderListOptions(list: string[], element: HTMLInputElement, optionsListContainer: HTMLElement): void{
     clearListOptions(optionsListContainer)
     if(list.length === 0){
         return
     }
     const inputGroup = optionsListContainer.appendChild(document.createElement('fieldset'))
-    inputGroup.innerHTML = list.map(option => /*html*/`<label for="${option}"><input name="${option}" data-dynamic-typeahead type="radio" value="${option}"/>${option}</label>`).join('')
+    inputGroup.innerHTML = list.map(option => /*html*/`<label for="${option}"><input name="${option}" id="${option}" data-dynamic-typeahead type="radio" value="${option}"/>${option}</label>`).join('')
     attachEventListenerToListOptions(Array.from(optionsListContainer.querySelectorAll('[data-dynamic-typeahead]')), element, optionsListContainer)
 }
